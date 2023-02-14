fce289da460dbd7c762f820dcdde957c0e2d6730
Sanchit <ssanchit960@gmail.com>
2023-02-14 17:18:08 +0530

    second

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import app from './app.js';
-import { conectDB } from "./db/conn.js";
+import { connectDB } from "./db/conn.js";
 import Razorpay from "razorpay";
 
-conectDB();
+connectDB();
 
 export var instance = new Razorpay({ key_id: process.env.RAZORPAY_API_KEY, key_secret: process.env.RAZORPAY_API_SECRET });
 
